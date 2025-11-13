78448a406abac576f15b79e590e9a57b5d7c4875
A-DREAM-YOUVE-HAD-BEFORE <li6620301@gmail.com>
2025-11-13 15:33:46 +0300

    Create FakeBoxingShuffle.lua

---
+++ FakeBoxingShuffle.lua
@@ -0,0 +1,25 @@
+local UserInputService = cloneref(game:GetService([[UserInputService]]))
+local LcPlayer = cloneref(game:FindService([[Players]]).LocalPlayer)
+
+
+local p_BoxingShuffle = (function()
+    for _, v in next, getgc(true) do
+        if type(v) == "table" and type(rawget(v, "Boxing Shuffle")) == "function" then
+            return v["Boxing Shuffle"]
+        end
+    end
+    return nil;
+end)()
+
+assert(p_BoxingShuffle, "UNKNOWN_FUNCTION");
+
+UserInputService.InputBegan:Connect(function(Input, _p)
+    if not _p and Input.KeyCode == Enum.KeyCode.Tab and LcPlayer.Character then
+        p_BoxingShuffle({
+            Windup_Speed = 0.8,
+            Windup_Duration = 0.4,
+            ExtraDashPower = 1,
+            Origin = LcPlayer.Character.HumanoidRootPart
+        });
+    end;
+end);
